e36b72186d039bc969666091b67a3fb2479c8f88
Nico <fossanico@gmail.com>
2018-05-14 18:05:12 +0200

    Modified:     Settings: added Javadoc and private constructor     Refactored deliveries Created:     SonarQube screenshoot

--- src/main/java/it/polimi/se2018/utils/Settings.java
+++ src/main/java/it/polimi/se2018/utils/Settings.java
@@ -1,12 +1,37 @@
 package it.polimi.se2018.utils;
 
+/**
+ * Settings for the game (constant values)
+ */
 public class Settings {
+    private Settings() {
+        //This constructor must be private because the class should not be instantiated from the outside
+    }
 
-  public static final int CARD_WIDTH = 5;
-  public static final int CARD_HEIGHT = 4;
-  public static final int MAX_DICE_PER_COLOR = 18;
-  public static final int TURNS = 10;
-  public static final int MAX_NUM_PLAYERS = 4;
-  public static final int MIN_NUM_PLAYERS = 2;
+    /**
+     * Width of the Schema
+     */
+    public static final int CARD_WIDTH = 5;
+
+    /**
+     * Height of the schema
+     */
+    public static final int CARD_HEIGHT = 4;
+    /**
+     * Result of 90 dices / {@link it.polimi.se2018.model.schema.GameColor}.values().length
+     */
+    public static final int MAX_DICE_PER_COLOR = 18;
+    /**
+     * Number of turns in a match
+     */
+    public static final int TURNS = 10;
+    /**
+     * Maximum number of players in a match
+     */
+    public static final int MAX_NUM_PLAYERS = 4;
+    /**
+     * Minimum number of players in a match
+     */
+    public static final int MIN_NUM_PLAYERS = 2;
 
 }
