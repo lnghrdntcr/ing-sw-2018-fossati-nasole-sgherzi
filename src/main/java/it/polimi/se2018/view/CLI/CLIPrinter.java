99df44c57ab0256f83e4b2495875d2d85af19533
Francesco Sgherzi <francesco.sgherzi@gmail.com>
2018-06-13 16:46:11 +0200

    Added CLIPrinter function NOT IMPLEMENTED

--- src/main/java/it/polimi/se2018/view/CLI/CLIPrinter.java
+++ src/main/java/it/polimi/se2018/view/CLI/CLIPrinter.java
@@ -1,10 +1,13 @@
 package it.polimi.se2018.view.CLI;
 
 import it.polimi.se2018.model.TurnHolder;
+import it.polimi.se2018.model.objectives.PublicObjective;
+import it.polimi.se2018.model.schema.Schema;
 import it.polimi.se2018.model.schema_card.SchemaCardFace;
 import it.polimi.se2018.model_view.DiceHolderImmutable;
 import it.polimi.se2018.model_view.DraftBoardImmutable;
 import it.polimi.se2018.model_view.PlayerImmutable;
+import it.polimi.se2018.model_view.ToolCardImmutable;
 import org.fusesource.jansi.AnsiConsole;
 
 import static org.fusesource.jansi.Ansi.Color.BLUE;
@@ -38,9 +41,22 @@ public class CLIPrinter {
         System.out.println(ansi().fg(RED).a(option).fg(BLUE).a(line).reset());
     }
 
-    public static void renderDraftboard(DraftBoardImmutable draftBoardImmutable) {
+    public static void printDraftBoard(DraftBoardImmutable draftBoardImmutable) {
     }
 
-    public static void renderRoundTrack(DiceHolderImmutable diceHolderImmutable) {
+    public static void printDiceHolder(DiceHolderImmutable diceHolderImmutable) {
+
     }
+
+    public static void printToolcard(ToolCardImmutable toolCardImmutable){
+
+    }
+
+    public static void printPublicObjectives(PublicObjective publicObjective) {
+    }
+
+    public static void printSchema(Schema schema){
+
+    }
+
 }
