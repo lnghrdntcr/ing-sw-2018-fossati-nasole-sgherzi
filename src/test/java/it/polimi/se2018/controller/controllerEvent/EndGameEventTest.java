e4627b0fd867f83837144f6946891e5f6c2a7ea0
Francesco Sgherzi <francesco.sgherzi@gmail.com>
2018-07-05 23:57:26 +0200

    Fixed random test fail

--- src/test/java/it/polimi/se2018/controller/controllerEvent/EndGameEventTest.java
+++ src/test/java/it/polimi/se2018/controller/controllerEvent/EndGameEventTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class EndGameEventTest {
     @Test
@@ -16,13 +16,13 @@ public class EndGameEventTest {
         scoreHolders.add(new ScoreHolder("AAA", 12, 82, 7, 3, 2));
         scoreHolders.add(new ScoreHolder("BBB", 121, 821, 71, 31, 21));
 
-        EndGameEvent event = new EndGameEvent("emitter", "receiver", "player", scoreHolders);
+        EndGameEvent event = new EndGameEvent("emitter", "receiver", "player", scoreHolders, "cia1");
 
 
         assertEquals(event.toJSON().toString(), Event.decodeJSON(event.toJSON().toString()).toJSON().toString());
 
-        assertEquals(event.getLeaderBoard().get(0).getTotalScore(), ((EndGameEvent)Event.decodeJSON(event.toJSON().toString())).getLeaderBoard().get(0).getTotalScore());
-        assertEquals(event.getLeaderBoard().get(1).getTotalScore(), ((EndGameEvent)Event.decodeJSON(event.toJSON().toString())).getLeaderBoard().get(1).getTotalScore());
+        assertEquals(event.getLeaderBoard().get(0).getTotalScore(), ((EndGameEvent) Event.decodeJSON(event.toJSON().toString())).getLeaderBoard().get(0).getTotalScore());
+        assertEquals(event.getLeaderBoard().get(1).getTotalScore(), ((EndGameEvent) Event.decodeJSON(event.toJSON().toString())).getLeaderBoard().get(1).getTotalScore());
     }
 
 }
