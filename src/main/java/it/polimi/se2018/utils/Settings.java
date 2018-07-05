0aad3b923419855ea6571c1b621653082147db17
Francesco Sgherzi <francesco.sgherzi@gmail.com>
2018-07-05 16:51:28 +0200

    Added possibility to play with custom schema card file, along the default ones

--- src/main/java/it/polimi/se2018/utils/Settings.java
+++ src/main/java/it/polimi/se2018/utils/Settings.java
@@ -4,15 +4,10 @@ package it.polimi.se2018.utils;
  * Settings for the game (constant values)
  */
 public class Settings {
-    private Settings() {
-        //This constructor must be private because the class should not be instantiated from the outside
-    }
-
     /**
      * Width of the Schema
      */
     public static final int CARD_WIDTH = 5;
-
     /**
      * Height of the schema
      */
@@ -33,27 +28,34 @@ public class Settings {
      * Minimum number of players in a match
      */
     public static final int MIN_NUM_PLAYERS = 2;
-
     /**
      * Number of toolcards in a match
      */
     public static final int TOOLCARDS_N = 3;
-
     /**
      * Number of public objectives in a match
      */
     public static final int POBJECTIVES_N = 3;
+    public final static String SOCKET_EOM = "#$$EOM$$#";
 
-    private static String schemaCardDatabase ="./gameData/resources/schemaCards/schemaCardBase.scf";
+    private static String defaultSchemaCardDatabase = "schemaCards/schemaCardBase.scf";
+
+    private static String customSchemaCardDatabase = "";
+
+    private Settings() {
+        //This constructor must be private because the class should not be instantiated from the outside
+    }
+
+    public static String getSchemaCardDatabase() {
+        return customSchemaCardDatabase;
+    }
 
     public static void setSchemaCardDatabase(String customSchemaCardPath) {
-        if(customSchemaCardPath==null || customSchemaCardPath.equals("")) return;
-        schemaCardDatabase = customSchemaCardPath;
+        if (customSchemaCardPath == null || customSchemaCardPath.equals("")) return;
+        customSchemaCardDatabase = customSchemaCardPath;
     }
 
-    public static String getSchemaCardDatabase(){
-        return schemaCardDatabase;
+    public static String getDefaultSchemaCardDatabase() {
+        return defaultSchemaCardDatabase;
     }
-
-    public final static String SOCKET_EOM="#$$EOM$$#";
 }
